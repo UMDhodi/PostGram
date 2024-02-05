ec887c0870f267f619218c09c7d0f938df582c8b
Uday Mayank Dhodi <103310535+UMDhodi@users.noreply.github.com>
2024-02-05 13:31:00 +0530

    change

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -26,7 +26,7 @@ const PostStats = ({ post, userId}: PostStatsProps) => {
 
   useEffect(() => {
     setIsSaved(!!savedPostRecord)
-  }, [currentUser])
+  }, [currentUser, savedPostRecord])
 
   const handleLikePost = (e: React.MouseEvent) => {
     e.stopPropagation();
